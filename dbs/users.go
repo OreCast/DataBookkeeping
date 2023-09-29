0337af063a15c7622c83d4a10e30497663f34481
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-29 13:20:49 -0400

    New user module

---
+++ dbs/users.go
@@ -0,0 +1,182 @@
+package dbs
+
+// nolint: gocyclo
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/OreCast/DataBookkeeping/utils"
+)
+
+// Users represents Users DBS DB table
+type Users struct {
+	USER_ID                int64  `json:"user_id"`
+	LOGIN                  string `json:"logical_user_name" validate:"required"`
+	FIRST_NAME             string `json:"first_name" validate:"required"`
+	LAST_NAME              string `json:"last_name"`
+	CREATION_DATE          int64  `json:"creation_date" validate:"required,number,gt=0"`
+	CREATE_BY              string `json:"create_by" validate:"required"`
+	LAST_MODIFICATION_DATE int64  `json:"last_modification_date" validate:"required,number,gt=0"`
+	LAST_MODIFIED_BY       string `json:"last_modified_by" validate:"required"`
+}
+
+// UserRecord represents user record pass to the users API
+type UserRecord struct {
+	UserId               string `json:"user_id"`
+	Login                string `json:"login" binding:"required"`
+	FirstName            string `json:"login" binding:"required"`
+	LastName             string `json:"login" binding:"required"`
+	Password             string `json:"login" binding:"required"`
+	CreationDate         int64  `json:"login" binding:"required"`
+	CreateBy             string `json:"login" binding:"required"`
+	LastModificationDate int64  `json:"login" binding:"required"`
+	LastModifiedBy       string `json:"login" binding:"required"`
+}
+
+// Users DBS API
+//gocyclo:ignore
+func (a *API) GetUser() error {
+	var args []interface{}
+	var conds []string
+	var err error
+
+	if len(a.Params) == 0 {
+		msg := "Users API with empty parameter map"
+		return Error(InvalidParamErr, ParametersErrorCode, msg, "dbs.users.Users")
+	}
+
+	tmpl := make(Record)
+	tmpl["Owner"] = DBOWNER
+	stm, err := LoadTemplateSQL("select_user", tmpl)
+	if err != nil {
+		return Error(err, LoadErrorCode, "", "dbs.users.Users")
+	}
+
+	stm = WhereClause(stm, conds)
+
+	// use generic query API to fetch the results from DB
+	err = executeAll(a.Writer, a.Separator, stm, args...)
+	if err != nil {
+		return Error(err, QueryErrorCode, "", "dbs.users.Users")
+	}
+	return nil
+}
+
+func (a *API) InsertUser() error {
+	return nil
+}
+func (a *API) UpdateUser() error {
+	return nil
+}
+func (a *API) DeleteUser() error {
+	return nil
+}
+
+// helper function to get next available UserID
+func getUserID(tx *sql.Tx) (int64, error) {
+	var err error
+	var tid int64
+	if DBOWNER == "sqlite" {
+		tid, err = LastInsertID(tx, "USERS", "user_id")
+		tid += 1
+	} else {
+		tid, err = IncrementSequence(tx, "SEQ_FL")
+	}
+	if err != nil {
+		return tid, Error(err, LastInsertErrorCode, "", "dbs.users.getUserID")
+	}
+	return tid, nil
+}
+
+// Insert implementation of Users
+func (r *Users) Insert(tx *sql.Tx) error {
+	var err error
+	if r.USER_ID == 0 {
+		userID, err := getUserID(tx)
+		if err != nil {
+			log.Println("unable to get userID", err)
+			return Error(err, ParametersErrorCode, "", "dbs.users.Insert")
+		}
+		r.USER_ID = userID
+	}
+	// set defaults and validate the record
+	r.SetDefaults()
+	err = r.Validate()
+	if err != nil {
+		log.Println("unable to validate record", err)
+		return Error(err, ValidateErrorCode, "", "dbs.users.Insert")
+	}
+	// get SQL statement from static area
+	stm := getSQL("insert_users")
+	if utils.VERBOSE > 0 {
+		log.Printf("Insert Users record %+v", r)
+	} else if utils.VERBOSE > 1 {
+		log.Printf("Insert Users\n%s\n%+v", stm, r)
+	}
+	_, err = tx.Exec(
+		stm,
+		r.USER_ID,
+		r.LOGIN,
+		r.FIRST_NAME,
+		r.LAST_NAME,
+		r.CREATION_DATE,
+		r.CREATE_BY,
+		r.LAST_MODIFICATION_DATE,
+		r.LAST_MODIFIED_BY)
+	if err != nil {
+		if utils.VERBOSE > 0 {
+			log.Println("unable to insert users, error", err)
+		}
+		return Error(err, InsertErrorCode, "", "dbs.users.Insert")
+	}
+	return nil
+}
+
+// Validate implementation of Users
+func (r *Users) Validate() error {
+	if err := RecordValidator.Struct(*r); err != nil {
+		return DecodeValidatorError(r, err)
+	}
+	if matched := unixTimePattern.MatchString(fmt.Sprintf("%d", r.CREATION_DATE)); !matched {
+		msg := "invalid pattern for creation date"
+		return Error(InvalidParamErr, PatternErrorCode, msg, "dbs.users.Validate")
+	}
+	if matched := unixTimePattern.MatchString(fmt.Sprintf("%d", r.LAST_MODIFICATION_DATE)); !matched {
+		msg := "invalid pattern for last modification date"
+		return Error(InvalidParamErr, PatternErrorCode, msg, "dbs.users.Validate")
+	}
+	return nil
+}
+
+// SetDefaults implements set defaults for Users
+func (r *Users) SetDefaults() {
+	if r.CREATION_DATE == 0 {
+		r.CREATION_DATE = Date()
+	}
+	if r.LAST_MODIFICATION_DATE == 0 {
+		r.LAST_MODIFICATION_DATE = Date()
+	}
+}
+
+// Decode implementation for Users
+func (r *Users) Decode(reader io.Reader) error {
+	// init record with given data record
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		log.Println("fail to read data", err)
+		return Error(err, ReaderErrorCode, "", "dbs.users.Decode")
+	}
+	err = json.Unmarshal(data, &r)
+
+	//     decoder := json.NewDecoder(r)
+	//     err := decoder.Decode(&rec)
+	if err != nil {
+		log.Println("fail to decode data", err)
+		return Error(err, UnmarshalErrorCode, "", "dbs.users.Decode")
+	}
+	return nil
+}
